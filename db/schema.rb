9d6557e58745f6e5da9a13c09e381a5f3ab11985
Sam Cheney <samcheneyus@gmail.com>
2020-09-08 14:30:32 +1000

    finished seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_070345) do
+ActiveRecord::Schema.define(version: 2020_09_07_072526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "total_price"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "human_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["human_id"], name: "index_bookings_on_human_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "humans", force: :cascade do |t|
     t.string "name"
@@ -24,6 +37,15 @@ ActiveRecord::Schema.define(version: 2020_09_07_070345) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_humans_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.integer "rating"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +61,8 @@ ActiveRecord::Schema.define(version: 2020_09_07_070345) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "humans"
+  add_foreign_key "bookings", "users"
   add_foreign_key "humans", "users"
+  add_foreign_key "reviews", "bookings"
 end
