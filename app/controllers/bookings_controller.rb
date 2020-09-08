fa19cec273bec7427918db61951972a9aa8e5190
Sam Cheney <samcheneyus@gmail.com>
2020-09-08 16:03:37 +1000

    merging to new code for signup

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,13 @@
 class BookingsController < ApplicationController
   
   def index
+    @bookings = Booking.where(user_id: current_user.id)
+    # @review = Review.new
   end
   
   def show
+    find_booking
+    @human = @booking.human
   end
   
   def new
@@ -14,16 +18,17 @@ class BookingsController < ApplicationController
   
   def edit
   end
-
+  
   def update
   end
-
+  
   def destroy
   end
-
+  
   private
-
-  def set_booking
+  
+  def find_booking
+    @booking = Booking.find(params[:id])
   end
 
   def booking_params
