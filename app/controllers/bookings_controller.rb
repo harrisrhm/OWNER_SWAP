506f43bf615cbb4c7eed619610008d117bd2220e
Sam Cheney <samcheneyus@gmail.com>
2020-09-08 16:40:21 +1000

    booking linked to show page skeleton

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,6 +11,7 @@ class BookingsController < ApplicationController
   end
   
   def new
+  
   end
   
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
+
+  get '/rails/info/routes' => 'routes'
+  
   resources :humans do
     resources :bookings
   end
