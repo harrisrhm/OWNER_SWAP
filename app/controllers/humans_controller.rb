f85a1f8553e8b684b75b610f3a24316c1d1cc33d
Eric Chen <eric19920108@gmail.com>
2020-09-08 10:23:29 +0800

    human update done

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -1,2 +1,19 @@
 class HumansController < ApplicationController
+    def edit
+        @human = Human.find(params[:id])
+      end
+
+    def update
+        @human = Human.find(params[:id])
+        @human.update(human_params)
+
+    # no need for app/views/humans/update.html.erb
+        redirect_to human_path(@human)
+    end
+
+    private
+
+    def human_params
+        params.require(:human).permit(:name, :description, :address, :rate)
+    end
 end
