7541f6a7b2f00cd4ec45b34131be486a25c25a9e
Eric Chen <eric19920108@gmail.com>
2020-09-09 20:52:45 +0800

    change the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,29 @@ puts "Creating humans"
 
 petuser = User.create!(name: "Bubbles", email: "test@gmail.com", password: "password")
 
-10.times do
-    human = Human.new(
-        name: Faker::FunnyName.name, 
-        description: Faker::ChuckNorris.fact,
-        address: Faker::Address.city,
-        rate: rand(1..100),
-        user: petuser
-    )
-    human.save!
+# 10.times do
+#     human = Human.new(
+#         name: Faker::FunnyName.name, 
+#         description: Faker::ChuckNorris.fact,
+#         address: Faker::Address.full_address ,
+#         rate: rand(1..100),
+#         user: petuser
+#     )
+#     human.save!
+# end
+eric = { name: "Eric", description: "give a lot of food", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+harris = { name: "Harris", description: "like scrach", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+sam = { name: "Sam", description: "like to play with me", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+haiden = { name: "Haiden", description: "cold-blood", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+bense = { name: "Bense", description: "like to tell joke", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+tiffany = { name: "Tiffany", description: "she only likes cats", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+sy = { name: "Sy", description: "he is noisey", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+lodi = { name: "Lodi", description: "good health skill", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+mathieu = { name: "Mathieu", description: "nice french guy", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+nico = { name: "Nico", description: "talking to you all day", address: Faker::Address.full_address, rate: rand(1..10), user: petuser }
+
+[eric, harris, sam, haiden, bense, tiffany, sy, lodi, mathieu, nico].each do |attributes|
+  human = Human.create!(attributes)
+  puts "Created #{human.name}"
 end
-puts "Finish!"
+puts "Finished!"
