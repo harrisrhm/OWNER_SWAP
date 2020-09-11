557f50e77d1ff85866c6192afafd4c38837c0605
Eric Chen <eric19920108@gmail.com>
2020-09-11 13:44:54 +0800

    fix seed photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,42 +24,38 @@ eric = Human.new(name: "Eric", description: "play with you all day", address: "T
 eric.photo.attach(io: file_eric, filename: 'eric.png', content_type: 'image/png')
 eric.save!
 
-file_harris = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHah_PNU-YUDw/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=Mg2Fct80h0uFL518bY0mpz4m0NmeMmtA9rw5EAna6Uc')
+file_harris = URI.open('https://avatars3.githubusercontent.com/u/59298084?v=4')
 harris = Human.new(name: "Harris", description: "play with you all day", address: "Melbourne", rate: rand(1..10), user: petuser3 )
 harris.photo.attach(io: file_harris, filename: 'harris.png', content_type: 'image/png')
 harris.save!
 
-file_nico = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQEHw0rXyVqRvw/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=JqrMb3OpvRzNJzhEtamROvAitdCTDDbj9khXcXSnoX8')
+file_nico = URI.open('https://avatars0.githubusercontent.com/u/43278026?v=4')
 nico = Human.new(name: "Nico", description: "play with you all day", address: "Barcelona", rate: rand(1..10), user: petuser3 )
 nico.photo.attach(io: file_nico, filename: 'nico.png', content_type: 'image/png')
 nico.save!
 
-file_tiffany = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQGRr8HV0yh0WA/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=iZuXwumQvafa0upSAzp0tuBiuY169F6j8JdDAEfV0Lw')
+file_tiffany = URI.open('https://avatars3.githubusercontent.com/u/67130477?v=4')
 tiffany = Human.new(name: "Tiffany", description: "play with you all day", address: "London", rate: rand(1..10), user: petuser3 )
 tiffany.photo.attach(io: file_tiffany, filename: 'tiffany.png', content_type: 'image/png')
 tiffany.save!
 
-file_lodi = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQGcMvBIkbvuwg/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=0K2GMjvy1Q_WNTrb-CD6liQqAb2v19nun0z0bY_fiak')
+file_lodi = URI.open('https://avatars1.githubusercontent.com/u/67579457?v=4')
 lodi = Human.new(name: "Lodi", description: "play with you all day", address: "Milan", rate: rand(1..10), user: petuser3 )
 lodi.photo.attach(io: file_lodi, filename: 'lodi.png', content_type: 'image/png')
 lodi.save!
 
-file_bence = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQG-jYm6Wmdbwg/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=hOVn1kacPfOS7cMANkGW_rnsMtOtthK5m-xoo8siISk')
+file_bence = URI.open('https://avatars2.githubusercontent.com/u/67093836?v=4')
 bence = Human.new(name: "Bence", description: "play with you all day", address: "Paris", rate: rand(1..10), user: petuser3 )
 bence.photo.attach(io: file_bence, filename: 'bence.png', content_type: 'image/png')
 bence.save!
 
-file_sharon = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHFDJDqgacF_g/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=8EH0HSNLxo76iIxK6l2rg8zrUgEy_584ErrspDF_aoM')
-sharon = Human.new(name: "Sharon", description: "play with you all day", address: "Praque", rate: rand(1..10), user: petuser3 )
-sharon.photo.attach(io: file_sharon, filename: 'sharon.png', content_type: 'image/png')
-sharon.save!
 
-file_mathieu = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQHzG6fy2PFuZQ/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=qNzB1LzrmOIpStG5HFHifrxGaSJ-_YhZDjOOAcWqJRY')
+file_mathieu = URI.open('https://avatars2.githubusercontent.com/u/64770169?v=4')
 mathieu = Human.new(name: "Mathieu", description: "play with you all day", address: "Nice", rate: rand(1..10), user: petuser3 )
 mathieu.photo.attach(io: file_mathieu, filename: 'mathieu.png', content_type: 'image/png')
 mathieu.save!
 
-file_sy = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQGAWvRcji7HZw/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=1Ja2RiKVnCis5rsKT1CiJjvxKwybMjoJU5byYGHjp8g')
+file_sy = URI.open('https://avatars2.githubusercontent.com/u/6656014?v=4')
 sy = Human.new(name: "Sy", description: "play with you all day", address: "Richmond, WA", rate: rand(1..10), user: petuser3 )
 sy.photo.attach(io: file_sy, filename: 'sy.png', content_type: 'image/png')
 sy.save!
