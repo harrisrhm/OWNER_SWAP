d1b641028b424af8bbae816cc66c8e32b41fb2c8
Harris Rahim <harris.rhm@gmail.com>
2020-09-11 15:57:25 +1000

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,17 @@ petuser2 = User.create!(name: "cat", email: "haiden@gmail.com", password: "passw
 petuser3 = User.create!(name: "gofer", email: "sam@gmail.com", password: "password")
 petuser4 = User.create!(name: "penguin", email: "harris@gmail.com", password: "password")
 
-file_sam = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHGZIQyFcDMwg/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=Jv8GYsDfFwikQgj1FQyyILWMk61L05fg27-WIDRZGYc')
+file_sam = URI.open('https://avatars1.githubusercontent.com/u/24800556?v=4')
 sam = Human.new(name: "Sam", description: "play with you all day", address: "Las Vegas", rate: rand(1..10), user: petuser3 )
 sam.photo.attach(io: file_sam, filename: 'sam.png', content_type: 'image/png')
-sam.save!
+sam.save!   
 
-file_eric = URI.open('https://media-exp1.licdn.com/dms/image/C5635AQESwIbYcVI5ig/profile-framedphoto-shrink_200_200/0?e=1599800400&v=beta&t=WL2y2C6uHUHYvKtZ1HfpVzsawXte-Mm5bi8AC0Vqmgs')
+file_eric = URI.open('https://avatars2.githubusercontent.com/u/43982345?v=4')
 eric = Human.new(name: "Eric", description: "play with you all day", address: "Taipei", rate: rand(1..10), user: petuser3 )
 eric.photo.attach(io: file_eric, filename: 'eric.png', content_type: 'image/png')
 eric.save!
 
-file_harris = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHah_PNU-YUDw/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=Mg2Fct80h0uFL518bY0mpz4m0NmeMmtA9rw5EAna6Uc')
+file_harris = URI.open('https://avatars3.githubusercontent.com/u/59298084?v=4')
 harris = Human.new(name: "Harris", description: "play with you all day", address: "Melbourne", rate: rand(1..10), user: petuser3 )
 harris.photo.attach(io: file_harris, filename: 'harris.png', content_type: 'image/png')
 harris.save!
@@ -34,27 +34,27 @@ nico = Human.new(name: "Nico", description: "play with you all day", address: "B
 nico.photo.attach(io: file_nico, filename: 'nico.png', content_type: 'image/png')
 nico.save!
 
-file_tiffany = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQGRr8HV0yh0WA/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=iZuXwumQvafa0upSAzp0tuBiuY169F6j8JdDAEfV0Lw')
+file_tiffany = URI.open('https://kitt.lewagon.com/alumni/tiff-o')
 tiffany = Human.new(name: "Tiffany", description: "play with you all day", address: "London", rate: rand(1..10), user: petuser3 )
 tiffany.photo.attach(io: file_tiffany, filename: 'tiffany.png', content_type: 'image/png')
 tiffany.save!
 
-file_lodi = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQGcMvBIkbvuwg/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=0K2GMjvy1Q_WNTrb-CD6liQqAb2v19nun0z0bY_fiak')
+file_lodi = URI.open('https://avatars1.githubusercontent.com/u/67579457?v=4')
 lodi = Human.new(name: "Lodi", description: "play with you all day", address: "Milan", rate: rand(1..10), user: petuser3 )
 lodi.photo.attach(io: file_lodi, filename: 'lodi.png', content_type: 'image/png')
 lodi.save!
 
-file_bence = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQG-jYm6Wmdbwg/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=hOVn1kacPfOS7cMANkGW_rnsMtOtthK5m-xoo8siISk')
+file_bence = URI.open('https://avatars2.githubusercontent.com/u/67093836?v=4')
 bence = Human.new(name: "Bence", description: "play with you all day", address: "Paris", rate: rand(1..10), user: petuser3 )
 bence.photo.attach(io: file_bence, filename: 'bence.png', content_type: 'image/png')
 bence.save!
 
-file_sharon = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHFDJDqgacF_g/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=8EH0HSNLxo76iIxK6l2rg8zrUgEy_584ErrspDF_aoM')
+file_sharon = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1595228790/crha1cnhlullhgc8nlgf.jpg')
 sharon = Human.new(name: "Sharon", description: "play with you all day", address: "Praque", rate: rand(1..10), user: petuser3 )
 sharon.photo.attach(io: file_sharon, filename: 'sharon.png', content_type: 'image/png')
 sharon.save!
 
-file_mathieu = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQHzG6fy2PFuZQ/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=qNzB1LzrmOIpStG5HFHifrxGaSJ-_YhZDjOOAcWqJRY')
+file_mathieu = URI.open('https://avatars2.githubusercontent.com/u/64770169?v=4')
 mathieu = Human.new(name: "Mathieu", description: "play with you all day", address: "Nice", rate: rand(1..10), user: petuser3 )
 mathieu.photo.attach(io: file_mathieu, filename: 'mathieu.png', content_type: 'image/png')
 mathieu.save!
